461b2ea3985a83b2eda85527034fc62a909aab4e
Sylvain <sylvain.joyeux@tidewise.io>
2021-08-23 13:32:34 -0300

    feat(cli): define 'status'

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,12 +10,21 @@ void usage(ostream& out) {
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         usage(cerr);
         exit(1);
     }
     Driver driver;
     driver.openURI(argv[1]);
+    driver.setReadTimeout(base::Time::fromSeconds(1));
+
+    string cmd = argv[2];
+
+    if (cmd == "status") {
+        driver.sendCommand(POWER_OFF, TORQUE_BUTTON, 0);
+        auto status = driver.read();
+        cout << status.power << " " << status.temperature << " " << status.cable_speed << " " << status.cable_length << std::endl;
+    }
 
     return 0;
 }
